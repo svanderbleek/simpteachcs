30d7949e12830c3e5d79c8aab57b1d6e9e705152
Sandy Vanderbleek <svanderbleek@Sandys-Mac-mini.attlocal.net>
2023-11-01 21:08:56 -0400

    permutations

---
+++ perm.py
@@ -0,0 +1,19 @@
+from math import factorial
+
+def permutations(elements):
+  if not elements:
+    raise Exception()
+  if len(elements) == 1:
+    return [elements]
+  generated = []
+  for index, start in enumerate(elements):
+    ends = permutations(elements[:index] + elements[index + 1:])
+    for end in ends:
+      generated.append([start] + end)
+  return generated
+
+arg = [1,2,3]
+result = permutations(arg)
+print(result)
+assert len(result) == factorial(len(arg)), "length should be factorial"
+print()
